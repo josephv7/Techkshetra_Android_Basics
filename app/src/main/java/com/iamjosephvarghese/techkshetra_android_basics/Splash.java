2424103df16d20783b62fd2fa77e1287f7e32acd
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 00:42:17 +0530

    :lipstick: Button click with toast

--- app/src/main/java/com/iamjosephvarghese/techkshetra_android_basics/Splash.java
+++ app/src/main/java/com/iamjosephvarghese/techkshetra_android_basics/Splash.java
@@ -3,13 +3,32 @@ package com.iamjosephvarghese.techkshetra_android_basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Toast.makeText(Splash.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
     }
 }
