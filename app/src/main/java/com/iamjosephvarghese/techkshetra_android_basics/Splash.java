e1c05b1a3ea587b9cbb8652165b0915f5258e934
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 00:44:30 +0530

    Intent added

--- app/src/main/java/com/iamjosephvarghese/techkshetra_android_basics/Splash.java
+++ app/src/main/java/com/iamjosephvarghese/techkshetra_android_basics/Splash.java
@@ -2,6 +2,7 @@ package com.iamjosephvarghese.techkshetra_android_basics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,8 +25,11 @@ public class Splash extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                Toast.makeText(Splash.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(Splash.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
 
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
 
             }
         });
